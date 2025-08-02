2c3402b8bee6278e453d7c1a116b46742effe6f2
Gilbert Lai <gilbertlmw@gmail.com>
2025-08-02 19:40:36 +1000

    init

--- environment/dev/project_wif.tf
+++ environment/dev/project_wif.tf
@@ -2,5 +2,5 @@ resource "google_service_account_iam_member" "github_actions_wif" {
   service_account_id = "projects/${var.project_id}/serviceAccounts/${google_service_account.gha.email}"
 
   role   = "roles/iam.workloadIdentityUser"
-  member = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github-actions-pool/attribute.repository/juktou/my-gcp-infra"
+  member = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/github-actions-pool/attribute.repository/gilbertlmw/my-gcp-infra"
 }

8408ba29c02f5a416de81aba9d64e14d8a73823d
Gilbert Lai <gilbertlmw@gmail.com>
2025-08-02 12:38:41 +1000

    init

---
+++ environment/dev/main.tf
@@ -0,0 +1,16 @@
+resource "google_storage_bucket" "default" {
+  name     = var.bucket_name
+  location = var.region
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+
+  lifecycle_rule {
+    condition {
+      age = 30
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
---
+++ environment/dev/terraform.tfvars
@@ -0,0 +1,2 @@
+project_id  = "stocks-415200"
+bucket_name = "my-tf-test-bucket-123-aaa"

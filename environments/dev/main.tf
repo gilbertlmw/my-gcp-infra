668a8f0a6b62e778272618108c065b62bc050589
Gilbert Lai <gilbertlmw@gmail.com>
2026-08-09 01:06:32 +1000

    init

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,42 +1,55 @@
-# 1. Instantiate Storage Module
-module "my-gcp-job-data-dev" {
+# 1a. Raw Data Bucket
+module "gcs_raw" {
   source = "../../modules/gcs"
 
   project_id    = var.project_id
   bucket_name   = "my-gcp-job-data-dev"
   location      = var.region
-  force_destroy = true # Dev safeguard setting
+  force_destroy = true
 }
 
-module "my-gcp-job-data-dev-1" {
+# 1b. Processed Data Bucket
+module "gcs_secondary" {
   source = "../../modules/gcs"
 
   project_id    = var.project_id
-  bucket_name   = "my-gcp-job-data-dev-1"
+  bucket_name   = "my-gcp-job-data-processed-dev"
   location      = var.region
-  force_destroy = true # Dev safeguard setting
+  force_destroy = true
 }
 
-# 2. Instantiate BigQuery Module
+# Preserve state if gcs was renamed to gcs_raw
+moved {
+  from = module.gcs
+  to   = module.gcs_raw
+}
+
+# 2. BigQuery Dataset
 module "bigquery" {
   source = "../../modules/bigquery"
 
   project_id                 = var.project_id
   dataset_id                 = "my_job_db_dev"
   location                   = var.region
-  delete_contents_on_destroy = true # Dev safeguard setting
+  delete_contents_on_destroy = true
 }
 
-# 3. Instantiate GHA Identity Module
+# 3. GHA Identity Module with Restricted IAM
 module "gha_identity" {
   source = "../../modules/gha_identity"
 
-  project_id               = var.project_id
-  project_number           = var.project_number
-  service_account_id       = "gha-deployer"
-  github_repo              = "gilbertlmw/my-gcp-infra"
+  project_id                = var.project_id
+  project_number            = var.project_number
+  gha_service_account_id    = "gha-deployer"
+  github_repo               = "gilbertlmw/my-gcp-infra"
   workload_identity_pool_id = "github-actions-pool"
 
-  target_bucket_name = "my-tf-state-bucket"
-  target_dataset_id  = "equity_research"
+  target_bucket_names = [
+    module.gcs_raw.bucket_name,
+    module.gcs_secondary.bucket_name
+  ]
+
+  target_dataset_ids = [
+    module.bigquery.dataset_id
+  ]
 }

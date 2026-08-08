58e1a2bb290322b54921f5721f7223e180865bb6
Gilbert Lai <gilbertlmw@gmail.com>
2026-08-08 20:21:14 +1000

    init

--- environment/dev/datasets.tf
+++ environment/dev/datasets.tf
@@ -7,3 +7,11 @@ resource "google_bigquery_dataset" "equity_research" {
     environment = "dev"
   }
 }
+
+resource "google_bigquery_dataset_access" "equity_research_access" {
+  dataset_id = google_bigquery_dataset.equity_research.dataset_id
+  project    = google_bigquery_dataset.equity_research.project
+
+  role          = "roles/bigquery.dataEditor"
+  user_by_email = "gha-deployer@stocks-415200.iam.gserviceaccount.com"
+}

c549287131cbcee11deb7370ae6e7e1ff5ee73f4
Gilbert Lai <gilbertlmw@gmail.com>
2026-08-08 23:28:05 +1000

    init

--- modules/gha_identity/main.tf
+++ modules/gha_identity/main.tf
@@ -1,48 +1,32 @@
-# 1. Create the Workload Identity Pool
-resource "google_iam_workload_identity_pool" "github_pool" {
-  workload_identity_pool_id = "github-pool"
-  display_name              = "GitHub Actions Pool"
-  description               = "Workload Identity Pool for GitHub Actions workflows"
-  project                   = var.project_id
-}
-
-# 2. Create the Workload Identity Provider
-resource "google_iam_workload_identity_pool_provider" "github_provider" {
-  workload_identity_pool_id          = google_iam_workload_identity_pool.github_pool.workload_identity_pool_id
-  workload_identity_pool_provider_id = "github-provider"
-  display_name                       = "GitHub Provider"
-  project                            = var.project_id
-
-  attribute_mapping = {
-    "google.subject"       = "assertion.sub"
-    "attribute.actor"      = "assertion.actor"
-    "attribute.repository" = "assertion.repository"
-  }
-
-  oidc {
-    issuer_uri = "https://token.actions.githubusercontent.com"
-  }
-}
-
-# 3. Create the Service Account
 resource "google_service_account" "gha_sa" {
   account_id   = var.service_account_id
-  display_name = "GitHub Actions Service Account (${var.service_account_id})"
+  display_name = "GitHub Actions Service Account"
   project      = var.project_id
 }
 
-# 4. Bind the Service Account to the Workload Identity Pool
 resource "google_service_account_iam_member" "workload_identity_user" {
   service_account_id = google_service_account.gha_sa.name
   role               = "roles/iam.workloadIdentityUser"
-  member             = "principalSet://iam.googleapis.com/projects/${var.project_id}/locations/global/workloadIdentityPools/${google_iam_workload_identity_pool.github_pool.workload_identity_pool_id}/attribute.repository/${var.github_repo}"
+
+  member = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/attribute.repository/${var.github_repo}"
 }
 
-# 5. Grant Resource IAM Permissions
-resource "google_storage_bucket_iam_member" "bucket_admin" {
-  bucket = var.target_bucket_name
-  role   = "roles/storage.objectAdmin"
-  member = "serviceAccount:${google_service_account.gha_sa.email}"
+resource "google_project_iam_member" "gha_bigquery_admin" {
+  project = var.project_id
+  role    = "roles/bigquery.admin"
+  member  = "serviceAccount:${google_service_account.gha_sa.email}"
+}
+
+resource "google_project_iam_member" "gha_storage_admin" {
+  project = var.project_id
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.gha_sa.email}"
+}
+
+resource "google_project_iam_member" "gha_compute_admin" {
+  project = var.project_id
+  role    = "roles/compute.admin"
+  member  = "serviceAccount:${google_service_account.gha_sa.email}"
 }
 
 resource "google_bigquery_dataset_iam_member" "dataset_editor" {
@@ -50,4 +34,17 @@ resource "google_bigquery_dataset_iam_member" "dataset_editor" {
   project    = var.project_id
   role       = "roles/bigquery.dataEditor"
   member     = "serviceAccount:${google_service_account.gha_sa.email}"
+}
+
+resource "google_storage_bucket_iam_member" "bucket_admin" {
+  bucket = var.target_bucket_name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.gha_sa.email}"
+}
+
+resource "google_project_iam_member" "gha_wif_bigquery_admin" {
+  project = var.project_id
+  role    = "roles/bigquery.admin"
+
+  member = "principalSet://iam.googleapis.com/projects/${var.project_number}/locations/global/workloadIdentityPools/${var.workload_identity_pool_id}/attribute.repository/${var.github_repo}"
 }
